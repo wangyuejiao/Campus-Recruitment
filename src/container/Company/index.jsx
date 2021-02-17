ae23e5d5716a7d47c5b3fb594f8b328948f72c13
zhangxintaobb <1309526743@qq.com>
2021-02-17 23:06:00 +0800

    company

--- src/container/Company/index.jsx
+++ src/container/Company/index.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
-import TopNav from '../../components/topNav'
+import React, { Component } from "react";
+import TopNav from "../../components/topNav";
 import RightNav from "../../components/RightNav/RightNav.js";
 import { Row, Col, Divider, Image, Button, Icon, Pagination } from "antd";
-import { Link } from 'react-router-dom'
-import qs from 'querystring'
+import { Link } from "react-router-dom";
+import qs from "querystring";
 
 export default class extends Component {
   constructor(props) {
@@ -11,127 +11,175 @@ export default class extends Component {
     this.state = {
       current: 1,
       num: 0,
-      company: []
-    }
+      company: [],
+    };
   }
-  onChange = page => {
+  onChange = (page) => {
     console.log(page);
     this.setState({
       current: page,
     });
     fetch("http://42.192.102.128:3000/company", {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Accept': "application/json,text/plain,*/*",
-        'Content-Type': 'application/x-www-form-urlencoded'
+        Accept: "application/json,text/plain,*/*",
+        "Content-Type": "application/x-www-form-urlencoded",
       },
       body: qs.stringify({
-        city: '1',
-        page: page
-      })
-
-    }).then(res => res.json())
-      .then(res => {
+        city: "1",
+        page: page,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
         this.setState({
-         company: res.list
-        })
-        console.log(res)
-      })
+          company: res.list,
+        });
+        console.log(res);
+      });
   };
   componentDidMount() {
     fetch("http://42.192.102.128:3000/company/companyNum", {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Accept': "application/json,text/plain,*/*",
-        'Content-Type': 'application/x-www-form-urlencoded'
+        Accept: "application/json,text/plain,*/*",
+        "Content-Type": "application/x-www-form-urlencoded",
       },
       body: qs.stringify({
-        city: '1',
-      })
-
-    }).then(res => res.json())
-      .then(res => {
+        city: "1",
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
         this.setState({
-          num: res.list[0].num
-        })
-        console.log(res)
-      })
+          num: res.list[0].num,
+        });
+        console.log(res);
+      });
     fetch("http://42.192.102.128:3000/company", {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Accept': "application/json,text/plain,*/*",
-        'Content-Type': 'application/x-www-form-urlencoded'
+        Accept: "application/json,text/plain,*/*",
+        "Content-Type": "application/x-www-form-urlencoded",
       },
       body: qs.stringify({
-        city: '1',
-        page: '1'
-      })
-
-    }).then(res => res.json())
-      .then(res => {
+        city: "1",
+        page: "1",
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
         this.setState({
-          company: res.list
-        })
-        console.log(res.list)
-      })
+          company: res.list,
+        });
+        console.log(res.list);
+      });
   }
 
   render() {
-    const { company } = this.state
+    const { company } = this.state;
+    console.log(this.state.company);
     return (
       <div>
-        <TopNav current='company' />
+        <TopNav current="company" />
         <Row>
           <Col span={23}>
-            <Row align='middle' style={{ backgroundColor: '#d0dde3', border: '1px solid #BBBBBB' }} >
-              <Col span={2} style={{ marginLeft: '1%' }}></Col>
-              <Col >
-                <Row style={{ marginTop: '30%' }}>公司地点:</Row>
+            <Row
+              align="middle"
+              style={{
+                backgroundColor: "#d0dde3",
+                border: "1px solid #BBBBBB",
+              }}
+            >
+              <Col span={2} style={{ marginLeft: "1%" }}></Col>
+              <Col>
+                <Row style={{ marginTop: "30%" }}>公司地点:</Row>
 
-                <Row style={{ marginTop: '30%' }}>融资阶段：</Row>
-                <Row style={{ marginTop: '30%' }}>公司规模：</Row>
-                <Row style={{ marginTop: '30%', marginBottom: '30%' }}>行业领域：</Row>
+                <Row style={{ marginTop: "30%" }}>融资阶段：</Row>
+                <Row style={{ marginTop: "30%" }}>公司规模：</Row>
+                <Row style={{ marginTop: "30%", marginBottom: "30%" }}>
+                  行业领域：
+                </Row>
               </Col>
             </Row>
-            <Row style={{ backgroundColor: '#F3F3F3' }} justify='space-around' align='middle'>
-              {company.map((item, index) => (
-                
-                
+            <Row
+              style={{ backgroundColor: "#F3F3F3", padding: "5% 10% 5% 10%" }}
+              justify="space-around"
+              align="middle"
+            >
+              {company.map((item,index)=>(
                   
-                    <Col span={5} style={{ backgroundColor: 'white', border: '1px solid #BBBBBB', marginTop: '3%', marginBottom: '3%', color: 'black' }}>
-                      <Row style={{ marginTop: '5%' }}>
-                      <Link to={{ pathname: '/companyinfo' }}>
-                        <Col span={10} >
-                          <img
-                            width='60%'
-                            height='80%'
-                            style={{ marginLeft: '10%', borderRadius: '10px' }}
-                            src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-                          />
-
-                        </Col>
-
-                        <Col style={{ marginLeft: '8%', color: 'black' }}>
-                          <Row justify="end" style={{ marginBottom: '10%', marginTop: '5%' }}>{item.company_name}</Row>
-                          <Row justify="end" style={{ fontSize: '12px' }}> {item.financing}<Divider type='vertical' style={{ backgroundColor: '#BBBBBB', marginTop: '4%' }} />{item.industry_area}</Row>
-                        </Col>
-                        </Link>
-                      </Row>
-                      
-                      <Divider type='horizontal'/>
-                      <Row style={{ marginLeft: '9%', fontSize: '12px', color: 'black' }}>在招岗位：{item.position_num}</Row>
-                      
+                   <Col span={7} style={{ backgroundColor: "white" ,border:'1px solid #B0E2FF',margin:'1% 0 1% 0'}}>
+                     
+                <Link to={{pathname:'companyinfo',search:'?code='+item.id}} style={{color:'black',display:'block'}}>
+                  <Row
+                    justify="space-between"
+                    align="center"
+                    style={{ margin: "10px" }}
+                  >
+                    <Col span={5}>
+                      <img
+                        src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+                        style={{
+                          margin: "0 auto",
+                          width: "100%",
+                          height: "100%",
+                          borderRadius:'10px'
+                        }}
+                      />
                     </Col>
+                    <Col span={16}>
+                      <Row justify="end" align="center">
+                        <h3>{item.company_name}</h3>
+                      </Row>
+                      <Row justify="end" align="center">
+                        <span>{item.financing}</span>
+                        <span>
+                          <Divider
+                            type="vertical"
+                            style={{ backgroundColor: "#BBBBBB" }}
+                          />
+                        </span>
+  
+                        <span>{item.industry_area}</span>
+                      </Row>
+                    </Col>
+                  </Row>
+                  <Row justify='center' align='center'>
+                    <Col span={22}>
+                    <Divider
+                    style={{
+                      backgroundColor: "#BBBBBB",
+                      height: "2px",
+                      margin: "0",
+                      width:'60%'
+                    }}
+                  />
+                    </Col>
+                 
+                  </Row>
                   
+                  <Row justify="start">
+                    <Col span={1}></Col>
+                    在招岗位：{item.position_num}
+                  </Row>
+                  </Link>
+                </Col>
                
-              ))
-              }
-
-
+              ))}
             </Row>
-            <Row style={{ backgroundColor: '#F3F3F3' }} justify='space-around' align='middle'>
-              <Pagination current={this.state.current} total={this.state.num} onChange={this.onChange} defaultPageSize={9}
-                style={{ marginTop: '4%', align: 'center' }} />
+            <Row
+              style={{ backgroundColor: "#F3F3F3" }}
+              justify="space-around"
+              align="middle"
+            >
+              <Pagination
+                current={this.state.current}
+                total={this.state.num}
+                onChange={this.onChange}
+                defaultPageSize={9}
+                style={{ marginTop: "4%", align: "center" }}
+              />
             </Row>
           </Col>
           <Col span={1}>
@@ -139,7 +187,6 @@ export default class extends Component {
           </Col>
         </Row>
       </div>
-    )
+    );
   }
 }
-
